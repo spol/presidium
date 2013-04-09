84626f11b827970d64df9fe3805d72f1af009708
Seb Pollard <seb@spolster.co.uk>
2013-04-09 16:40:24 +0100

    Updated deployment config.

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,11 +20,19 @@ set :use_sudo, false
 # Define servers to deploy to. These can be moved to environment files if different per env.
 server 'talk.spol.co', :web
 
+set :db_name, "presidium"
+set :db_user, "presidium"
+set :db_pass, "presidium"
+
 set :group_writable, false
 
 # deployment process
+after "deploy:setup", "mysql:init"
+
 after "deploy:update_code", "deploy:create_symlinks"
 after "deploy:update_code", "php:composer"
+after "deploy:update_code", "apache:update_vhost"
+after "deploy:update_code", "laravel:migrate"
 
 
 namespace :deploy do
@@ -34,15 +42,49 @@ namespace :deploy do
 		# link in the uploads directory
 		#run "rm -r #{current_release}/media"
 		#run "ln -s #{deploy_to}/#{shared_dir}/media #{current_release}/media"
-		#run "ln -s #{deploy_to}/#{shared_dir}/wkhtmltox #{current_release}/bin/wkhtmltox"
+	end
+end
+
+namespace :apache do
+
+	desc "Update/Create Vhost file"
+	task :update_vhost, :roles => :web do
+		run "cp #{current_release}/config/apache/talk.spol.co.conf /etc/apache2/sites-available/talk.spol.co"
+		run "sudo /usr/sbin/a2ensite talk.spol.co"
+		run "sudo /usr/sbin/apachectl graceful"
+	end
+
+end
+
+namespace :mysql do
+
+	desc "Create application mysql user and db"
+	task :init, :roles => :web do
+		db_name = fetch(:db_name)
+		db_user = fetch(:db_user)
+		db_pass = fetch(:db_pass)
+		root_pw = fetch(:mysql_root_pw)
+		run "mysql -uroot -p#{root_pw} -e \"GRANT ALL PRIVILEGES ON #{db_name}.* TO '#{db_user}'@'localhost' IDENTIFIED BY '#{db_pass}'\""
+
 	end
 
 end
 
 namespace :php do
+
 	desc "Install composer dependancies"
 	task :composer, :roles => :web do
-		run "cd #{current_release} && mkdir bin && wget -qO- http://getcomposer.org/installer | php -- --install-dir=bin"
-		run "cd #{current_release} && php #{current_release}/bin/composer.phar install --no-progress -o"
+#		run "cd #{current_release} && mkdir bin && wget -qO- http://getcomposer.org/installer | php -- --install-dir=bin"
+		run "cd #{current_release} && composer install --no-progress -o"
 	end
+
+end
+
+namespace :laravel do
+
+	desc "Run artisan migrations"
+	task :migrate, :roles => :web do
+		run "cd #{current_release} && php artisan migrate"
+	end
+
 end
--- config/deploy/prd.rb
+++ config/deploy/prd.rb
@@ -2,4 +2,6 @@ set :deploy_to, "/var/www/talk.spol.co"
 
 set :environment, "Production"
 
+set :mysql_root_pw, "QuJH92G842viu4V"
+
 after "deploy", "deploy:cleanup"
